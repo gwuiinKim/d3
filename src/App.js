6d39bb9c02e49f4e4f389c8fa12c9cab4d7d5920
gwuiinkim <gwuiinkim@gmail.com>
2019-07-04 19:55:14 +0900

    data import and data handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import * as d3 from "d3";
+import _ from "lodash";
+
+import Preloader from "./Components/Preloader";
+
+import { loadAllData } from "./DataHandling";
 
 function App() {
-  return <div className="App" />;
+  const [state, setState] = useState({
+    techSalaries: [],
+    medianIncomes: [],
+    countyNames: []
+  });
+
+  useEffect(() => {
+    loadAllData(data => setState(data));
+  });
+
+  const { techSalaries } = state;
+
+  if (techSalaries.length < 1) {
+    return <Preloader />;
+  }
+  return (
+    <div className="App">
+      <div className="App container">
+        <h1>Loaded {techSalaries.length} salaries</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
